773aeedd66812cf523210af9c8668867c61f78dd
prashant-matrix <72510700+prashant-matrix@users.noreply.github.com>
2023-06-19 19:33:21 +0545

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ resource "aws_vpc" "prashant-testvpc" {
 
 # Define the subnets
 resource "aws_subnet" "sub-ap-south-1a" {
-  vpc_id = aws_vpc.testvpc.id
+  vpc_id = aws_vpc.prashant-testvpc.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "ap-south-1a"
   map_public_ip_on_launch = true
